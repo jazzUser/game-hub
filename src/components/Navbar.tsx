3f3a98c474e356c19dbad4fae0542342904b99b7
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-12-09 18:58:07 +0100

    create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Image, HStack, Text } from '@chakra-ui/react'
+import logo from '../assets/logo.webp'
+
+const Navbar = () => {
+  return (
+    <HStack>
+        <Image src={logo} boxSize="60px"></Image>
+        <Text>Navbar</Text>
+    </HStack>
+  )
+}
+
+export default Navbar
