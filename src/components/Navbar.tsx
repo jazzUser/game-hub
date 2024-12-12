2cb084db71677062e83023e9d8129f9319bca6ef
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-12-12 12:14:40 +0100

    implement searching

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,16 @@ import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
 
-const Navbar = () => {
+interface Props {
+  onSearch: (searchtext: string) => void;
+}
+
+
+const Navbar = ({onSearch}: Props) => {
   return (
     <HStack padding='10px'>
         <Image src={logo} boxSize="60px"></Image>
-        <SearchInput></SearchInput>
+        <SearchInput onSearch={onSearch}></SearchInput>
         <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   )
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -26,6 +25,7 @@ const useGames = (
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
       },
     },
     [gameQuery]
